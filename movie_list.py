7124352b73a0552625366899832e073c892c6e9d
velko4 <vrizov5@gmail.com>
2025-12-07 22:35:04 +0200

    changing the group by to order by in the queries

--- movie_list.py
+++ movie_list.py
@@ -1,11 +1,11 @@
-from variables import check_output
 from db_connection import get_connection
+from variables import check_output
 
 
 def list_of_movies():
     conn, cur = get_connection()
     print("Here is what we have stored in the library")
-    cur.execute('SELECT MOVIE_TITLE FROM movie GROUP BY ID')
+    cur.execute('SELECT MOVIE_TITLE FROM movie ORDER BY ID')
     results = cur.fetchall()
     check_output(results)  # called from variables
     conn.close()
@@ -14,7 +14,7 @@ def list_of_movies():
 def list_of_movies_admin():
     conn, cur = get_connection()
     print("Here is what we have stored in the library")
-    cur.execute('SELECT ID, MOVIE_TITLE FROM movie GROUP BY ID')
+    cur.execute('SELECT ID, MOVIE_TITLE FROM movie ORDER BY ID')
     results = cur.fetchall()
     check_output(results)  # called from variables
     conn.close()
