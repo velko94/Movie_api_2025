9cbc469f76f196ae06ecc6a44adb9f69a5cada89
velko4 <vrizov5@gmail.com>
2025-12-08 01:51:21 +0200

    making the admin menu return for more after you do something in it which was anoying for all the debuging of the program
    fixing the run_update db connection
    
    fixing the movie id to not use global and run untill the user inputs correct id
    fixed all the update fucntions to remove the globals from variable module
    rewriten the admin delete record to fix the recursion and moved the message for deletion for better clarity

--- admin_options.py
+++ admin_options.py
@@ -3,8 +3,6 @@ import db_actions
 import db_connection
 import movie_list
 
-global movid
-
 
 def admin_menu():
     print("choice 1:want to list all movies")
@@ -18,34 +16,34 @@ def admin_menu():
 
 # The functionalities for the admin menu
 def admin_actions():
-
-    choice = input("What do you want to do use only digits ", )
-    if choice == '1':
-        movie_list.list_of_movies_admin()
-        print("choice 1:want to list all movies")
-    elif choice == '2':
-        print("Soo choice 2:want to recreate a  table")
-        db_actions.recreate_existing_table()
-    elif choice == '3':
-        print("choice 3:want to delete a whole table")
-        db_actions.table_delete()
-    elif choice == '4':
-        print("choice 4: So want to delete a record from a table")
-        admin_delete_record()
-    elif choice == '5':
-        print("choice 5:want to change something in a record")
-        admin_update()
-    elif choice == '6':
-        print("WELCOME THE THE USER MENU")
-        from movie_api_GUI import actions
-        actions()
-    else:
-        print("WRONG CHOICE TRY AGAIN")
-        admin_menu()
+    while True:
+        choice = input("What do you want to do use only digits ", )
+        if choice == '1':
+            movie_list.list_of_movies_admin()
+            print("choice 1:want to list all movies")
+        elif choice == '2':
+            print("Soo choice 2:want to recreate a  table")
+            db_actions.recreate_existing_table()
+        elif choice == '3':
+            print("choice 3:want to delete a whole table")
+            db_actions.table_delete()
+        elif choice == '4':
+            print("choice 4: So want to delete a record from a table")
+            admin_delete_record()
+        elif choice == '5':
+            print("choice 5:want to change something in a record")
+            admin_update()
+        elif choice == '6':
+            print("WELCOME THE THE USER MENU")
+            from movie_api_GUI import actions
+            actions()
+            return
+        else:
+            print("WRONG CHOICE TRY AGAIN")
 
 
 def run_update(query, params):
-    conn, cur = db_actions.get_connection()
+    conn, cur = db_connection.get_connection()
     try:
         cur.execute(query, params)
         conn.commit()
@@ -56,43 +54,43 @@ def run_update(query, params):
 # FUNCTIONS FOR UPDATES ON EXISTING RECORDS#
 
 def movie_id():
-    global movid
-    movid = input("What is the id: ")
-    if not movid.isdigit():
+    while True:
+        movid = input("What is the id: ").strip()
+        if movid.isdigit():
+            return int(movid)
+
         print("ID must be a number.")
-        return movie_id()
-    return int(movid)
 
 
 def update_name():
     print("Change the name to the value you wish to be")
-    variables.name()
-    movie_id()
-    run_update("UPDATE movie SET MOVIE_TITLE=? WHERE ID=?;", [variables.movie_name, movid])
+    new_name = variables.name()
+    id_value = movie_id()
+    run_update("UPDATE movie SET MOVIE_TITLE=? WHERE ID=?;", [new_name, id_value])
 
 
 def update_genre():
-    variables.input_genre()
-    movie_id()
-    run_update("UPDATE movie SET GENRE=?  WHERE ID=? ", [variables.genre, movid])
+    new_genre = variables.input_genre()
+    id_value = movie_id()
+    run_update("UPDATE movie SET GENRE=?  WHERE ID=? ", [new_genre, id_value])
 
 
 def update_director():
-    variables.director_name()
-    movie_id()
-    run_update("UPDATE movie SET DIRECTOR=?  WHERE ID=? ", [variables.direct, movid])
+    new_dir = variables.director_name()
+    id_value = movie_id()
+    run_update("UPDATE movie SET DIRECTOR=?  WHERE ID=? ", [new_dir, id_value])
 
 
 def update_desc():
-    variables.description()
-    movie_id()
-    run_update("UPDATE movie SET DESCRIPTION=?  WHERE ID=? ", [variables.description, movid])
+    new_desc = variables.description()
+    id_value = movie_id()
+    run_update("UPDATE movie SET DESCRIPTION=?  WHERE ID=? ", [new_desc, id_value])
 
 
 def update_year():
-    variables.release_year()
-    movie_id()
-    run_update("UPDATE movie SET RELEASE_YEAR=?  WHERE ID=? ", [variables.year, movid])
+    new_year = variables.release_year()
+    id_value = movie_id()
+    run_update("UPDATE movie SET RELEASE_YEAR=?  WHERE ID=? ", [new_year, id_value])
 
 
 # THis is the engine for updating a single record in the db and uses above functions
@@ -115,23 +113,25 @@ def admin_update():
 
 # Deleting just a record from the table
 def admin_delete_record():
-    allowed_tables = db_actions.check_existing_tables()
-    table_to_delete = input("What is the name of the table: ")
-    if table_to_delete not in allowed_tables:
-        print("Invalid table name!")
-        return admin_delete_record()
-    movies_id = movie_id()
-    conn, cur = db_connection.get_connection()
-    cur.execute(f"SELECT 1 FROM {table_to_delete} WHERE ID = ?", (movies_id,))
-    result = cur.fetchone()
-    if not result:
-        print(f"No record with ID {movies_id} in table {table_to_delete}.")
-        conn.close()
-        return admin_delete_record()
-    else:
+    while True:
+        allowed_tables = [t.lower() for t in db_actions.check_existing_tables()]
+        table_to_delete = input("What is the name of the table: ").strip().lower()
+        if table_to_delete not in allowed_tables:
+            print("Invalid table name!")
+            continue
+
+        movies_id = movie_id()
+        conn, cur = db_connection.get_connection()
+        cur.execute(f"SELECT 1 FROM {table_to_delete} WHERE ID = ?", (movies_id,))
+        result = cur.fetchone()
+
+        if not result:
+            print(f"No record with ID {movies_id} in table {table_to_delete}.")
+            conn.close()
+            continue
+
         cur.execute(f"DELETE FROM {table_to_delete} WHERE ID = ?;", (movies_id,))
-        print("record", movies_id, "was deleted  from", table_to_delete)
-        # To reset the id's and keep them in correct order
-        cur.execute("DELETE FROM sqlite_sequence WHERE name='movie'")
         conn.commit()
         conn.close()
+        print("record", movies_id, "was deleted  from", table_to_delete)
+        return
