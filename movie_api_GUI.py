6e38fa4002e7183142f0d5c78a71beac9b1f09a4
velko4 <vrizov5@gmail.com>
2025-12-08 02:59:37 +0200

    fixing the imports and the recursion on the menu also converting the choice to alway be in the lower case
    fixing the recursion on the all done as well and a typo in invalid on line 69

--- movie_api_GUI.py
+++ movie_api_GUI.py
@@ -1,76 +1,82 @@
 import sys
-from movie_add import adding
-from movie_search import search_menu
-from movie_details import details
-from movie_ranglist import ranglist_menu
-from movie_list import list_of_movies
+
 import admin_options
+import movie_add
+import movie_details
+import movie_list
+import movie_ranglist
+import movie_search
+import users
 import variables
-from users import check_user
 
 
 def actions(choice=None):
-    if choice is None:
-        variables.menu()
-        choice = input("What do you want to do ", ).strip().lower()
-    else:
-        choice = choice.lower()
-    if choice in ['movlst', '--movlst']:
-        print("Listing all movies")
-        list_of_movies()
-        all_done()
-    elif choice in ['movdt', '--movdt']:
-        print("Showing movies details")
-        details()
-        all_done()
-    elif choice in ['movsrch', '--movsrch']:
-        print("Searching for a movie")
-        search_menu()
-        all_done()
-    elif choice in ['movadd', '--movadd']:
-        print("Adding a movie")
-        adding()
-        all_done()
-    elif choice in ['movfv', '--movfv']:
-        print("Opening favorites!")
-        # from users call check user variable\
-        check_user()
-        all_done()
-    elif choice in ['movcat', '--movcat']:
-        print("Showing top 5 movies")
-        ranglist_menu()  # from movie_ranglist
-        all_done()
-    elif choice in ['movadm', '--movadm']:
-        print("Showing ADMIN MENU")
-        admin_options.admin_menu()
-        all_done()
-    elif choice == "exit":
-        print("Alright, have a nice day!")
-        sys.exit(0)
-    else:
-        variables.wrong_choice()
-        if choice.startswith('--'):
-            sys.exit(1)
+    while True:
+
+        if choice is None:
+            variables.menu()
+            choice = input("What do you want to do ", ).strip().lower()
         else:
-            actions()
+            choice = choice.lower()
+
+        if choice in ['movlst', '--movlst']:
+            print("Listing all movies")
+            movie_list.list_of_movies()
+
+        elif choice in ['movdt', '--movdt']:
+            print("Showing movies details")
+            movie_details.details()
+
+        elif choice in ['movsrch', '--movsrch']:
+            print("Searching for a movie")
+            movie_search.search_menu()
+
+        elif choice in ['movadd', '--movadd']:
+            print("Adding a movie")
+            movie_add.adding()
+
+        elif choice in ['movfv', '--movfv']:
+            print("Opening favorites!")
+            # from users call check user variable\
+            users.check_user()
+
+        elif choice in ['movcat', '--movcat']:
+            print("Showing top 5 movies")
+            movie_ranglist.ranglist_menu()  # from movie_ranglist
+
+        elif choice in ['movadm', '--movadm']:
+            print("Showing ADMIN MENU")
+            admin_options.admin_menu()
+
+        elif choice == "exit":
+            print("Alright, have a nice day!")
+            sys.exit(0)
+        else:
+            variables.wrong_choice()
+            if choice.startswith('--'):
+                sys.exit(1)
+            choice = None
+            continue
 
 
 # asking the person if he wants to continue
 
 def all_done():
-    done = input("Want to do something more? ").strip().lower()
-    if done == 'no':
-        print("Alright have a nice day")
-        sys.exit(0)
-    elif done == 'yes':
-        actions()
-    else:
-        variables.wrong_choice()
-        print("Ivalid choice please type yes or no")
-        all_done()
+    while True:
+        done = input("Want to do something more? ").strip().lower()
+
+        if done == 'no':
+            print("Alright have a nice day")
+            return False
+        elif done == 'yes':
+            return True
+        else:
+            variables.wrong_choice()
+            print("Invalid choice please type yes or no")
+            all_done()
 
 
-#Used so that the main command can be called from the terminal directly
+# Used so that the main command can be called from the terminal directly
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         # Take the first CLI argument as the choice
