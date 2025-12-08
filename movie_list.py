0e64fff210b013e5d60e096e9f6c36128dc9ec36
velko4 <vrizov5@gmail.com>
2025-12-08 03:14:59 +0200

    fixing the imports and removing unnecessary comments

--- movie_list.py
+++ movie_list.py
@@ -1,20 +1,20 @@
-from db_connection import get_connection
-from variables import check_output
+import variables
+import db_connection
 
 
 def list_of_movies():
-    conn, cur = get_connection()
+    conn, cur = db_connection.get_connection()
     print("Here is what we have stored in the library")
     cur.execute('SELECT MOVIE_TITLE FROM movie ORDER BY ID')
     results = cur.fetchall()
-    check_output(results)  # called from variables
+    variables.check_output(results)
     conn.close()
 
 
 def list_of_movies_admin():
-    conn, cur = get_connection()
+    conn, cur = db_connection.get_connection()
     print("Here is what we have stored in the library")
     cur.execute('SELECT ID, MOVIE_TITLE FROM movie ORDER BY ID')
     results = cur.fetchall()
-    check_output(results)  # called from variables
+    variables.check_output(results)
     conn.close()
