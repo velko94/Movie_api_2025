8e7fbc865ee43643ad225e36d92b08eff0ee6d4b
velko4 <vrizov5@gmail.com>
2025-12-08 03:50:21 +0200

    fixing imports and recursion

--- movie_search.py
+++ movie_search.py
@@ -1,5 +1,5 @@
-from db_connection import get_connection
-from variables import check_output, name, input_genre, description, director_name, release_year, wrong_choice
+import db_connection
+import variables
 
 
 def search_menu():
@@ -13,49 +13,62 @@ def search_menu():
 
 
 def search_engine():
-    conn, cur = get_connection()
-    search_by = input("please choose by what parameter to search for ").strip().lower()
-    if search_by == 'name':
-        search_parameter = name()
-        cur.execute(
-            "SELECT * FROM movie WHERE MOVIE_TITLE LIKE ?;",
-            ["%" + search_parameter + "%", ])
-        results = cur.fetchall()
-        check_output(results)
-        conn.close()
-    elif search_by == 'genre':
-        search_parameter = input_genre()
-        cur.execute(
-            "SELECT * FROM movie WHERE GENRE LIKE ?;",
-            ["%" + search_parameter + "%"])
-        results = cur.fetchall()
-        check_output(results)
-        conn.close()
-    elif search_by == 'desc':
-        search_parameter = description()
-        cur.execute(
-            "SELECT *  FROM movie WHERE DESCRIPTION LIKE ? ;",
-            ["%" + search_parameter + "%"])
-        results = cur.fetchall()
-        check_output(results)
-        conn.close()
-    elif search_by == 'dir':
-        search_parameter = director_name()
-        cur.execute(
-            "SELECT * FROM movie WHERE DIRECTOR LIKE ? ;",
-            ["%" + search_parameter + "%"])
-        results = cur.fetchall()
-        check_output(results)
-        conn.close()
-    elif search_by == 'year':
-        search_parameter = release_year()
-        cur.execute(
-            "SELECT * FROM movie WHERE RELEASE_YEAR = ?  ;",
-            [search_parameter])
-        results = cur.fetchall()
-        check_output(results)
-        conn.close()
-    else:
-        print("Please use the parameters name,genre,desc,dir,year")
-        wrong_choice()
-        search_engine()
+    while True:
+        search_by = input("please choose by what parameter to search for ").strip().lower()
+        conn, cur = db_connection.get_connection()
+
+        if search_by == 'name':
+            search_parameter = variables.name()
+            cur.execute(
+                "SELECT * FROM movie WHERE MOVIE_TITLE LIKE ?;",
+                ["%" + search_parameter + "%", ])
+            results = cur.fetchall()
+            variables.check_output(results)
+            conn.close()
+            break
+
+        elif search_by == 'genre':
+            search_parameter = variables.input_genre()
+            cur.execute(
+                "SELECT * FROM movie WHERE GENRE LIKE ?;",
+                ["%" + search_parameter + "%"])
+            results = cur.fetchall()
+            variables.check_output(results)
+            conn.close()
+            break
+
+        elif search_by == 'desc':
+            search_parameter = variables.description()
+            cur.execute(
+                "SELECT *  FROM movie WHERE DESCRIPTION LIKE ? ;",
+                ["%" + search_parameter + "%"])
+            results = cur.fetchall()
+            variables.check_output(results)
+            conn.close()
+            break
+
+        elif search_by == 'dir':
+            search_parameter = variables.director_name()
+            cur.execute(
+                "SELECT * FROM movie WHERE DIRECTOR LIKE ? ;",
+                ["%" + search_parameter + "%"])
+            results = cur.fetchall()
+            variables.check_output(results)
+            conn.close()
+            break
+
+        elif search_by == 'year':
+            search_parameter = variables.release_year()
+            cur.execute(
+                "SELECT * FROM movie WHERE RELEASE_YEAR = ?  ;",
+                [search_parameter])
+            results = cur.fetchall()
+            variables.check_output(results)
+            conn.close()
+            break
+
+        else:
+            print("Please use the parameters name,genre,desc,dir,year")
+            variables.wrong_choice()
+            conn.close()
+            continue
